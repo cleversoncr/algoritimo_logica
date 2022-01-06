63d1a528a2e9bd315e69274f48f980ba53ad0479
Cleverson Rocha <cleverson.rocha@minu.co>
2022-01-06 15:25:34 -0300

    sem ssh

---
+++ fundamentos/01_inicio/2_sentenca_2.js
@@ -0,0 +1,13 @@
+//Comentários em linha
+
+/*
+Comentários em bloco
+#1
+#2
+*/
+
+console.log(1 + 3);
+console.log('Cleverson Rocha');
+console.log('Passo #01');
+console.log('Passo #02');
+console.log('Passo #03');
---
+++ fundamentos/01_inicio/3_comentario.js
@@ -0,0 +1,10 @@
+//Console.log é usado para exibir (imprimir) algo na tela do computador
+console.log("Print");
+console.log(2 + 3); //mais um exemplo de console.log
+// console.log('Linha comentada')
+
+/*
+  Esse é um comentári
+  de multiplas linhas!
+*/
+console.log('passo #3')
